f92180d9d7f25a4e9d6e86227b081c85a4432789
Forejy <joffrey.marques@gmail.com>
2021-11-16 23:09:50 +0100

    v1 site operationnelle

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const devBundle = require('./devBundle') //To comment in production
 const path = require('path')
 const template = require('./../template')
 const { MongoClient } = require('mongodb')
+require('dotenv').config()
 
 const app = express()
 devBundle.compile(app) //To comment in production
 
 const CURRENT_WORKING_DIR = process.cwd()
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR), 'dist'))
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
 
 app.get('/', (req, res) => {
 	res.status(200).send(template())
@@ -22,8 +24,8 @@ app.listen(port, (err) => {
 	console.info('Server started on port %s.', port)
 })
 
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup'
-MongoClient.connect(url, (err, db) => {
+const url = process.env.MONGODB_URI
+MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true }, (err, db) => {
 	console.log("Connected successfully to mongodb server")
+	db.close()
 })
-db.close()
